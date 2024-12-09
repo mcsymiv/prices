1c3ea73f4dbeb572452f11e450e1b18b6ae3da3b
mcsymiv <s.mcsymiv@gmail.com>
2024-12-09 18:27:33 +0200

    upd logic

--- src/util/prices.ts
+++ src/util/prices.ts
@@ -42,26 +42,22 @@ export async function comparePrices(prev: Item[], next: Item[]): Promise<Item[]>
   for (const name in oldItems) {
     const item: Item = newItems[name]
 
-
     if (oldItems[name].promo === 0 && newItems[name].promo !== 0 && (oldItems[name].regular !== newItems[name].regular)) {
       // product is in promo with regular price change
 
       const diff: number = calculateDiff(newItems[name].promo, newItems[name].regular)
       await bot.send(`Продукт "${item.name}" зайшов у промо "${diff}", змінивши регулярну ціну. Попередня: ${oldItems[name].regular}. Тепер: ${newItems[name].regular}`)
-      item.changed = true
       updatedPrices.push(item)
 
     } else if (oldItems[name].promo !== 0 && newItems[name].promo === 0 && (oldItems[name].regular === newItems[name].regular)) {
       // product is out of promo without price change
       await bot.send(`Продукт "${item.name}" вийшов з промо, без зміни регулярної ціни: ${newItems[name].regular}. Попереднє промо: ${oldItems[name].promo}`)
 
-      item.changed = true
       updatedPrices.push(item)
-    } else if ((oldItems[name].regular !== newItems[name].regular) && newItems[name].promo == 0 && oldItems[name].promo !== 0) {
+    } else if (oldItems[name].promo !== 0 && newItems[name].promo === 0 && (oldItems[name].regular !== newItems[name].regular) ) {
       // product is out of promo with price change
 
       await bot.send(`Продукт "${item.name}" вийшов з промо змінивши регулярну ціну: ${newItems[name].regular}`)
-      item.changed = true
       updatedPrices.push(item)
 
     } else if (oldItems[name].promo !== newItems[name].promo) {
@@ -69,27 +65,23 @@ export async function comparePrices(prev: Item[], next: Item[]): Promise<Item[]>
 
       const diff: number = calculateDiff(oldItems[name].promo, newItems[name].promo)
       await bot.send(`Продукт "${item.name}" змінив промо ціну на "${diff}". Попередня: ${oldItems[name].promo}. Тепер: ${newItems[name].promo}`)
-      item.changed = true
       updatedPrices.push(item)
 
     } else if (oldItems[name].regular !== newItems[name].regular) {
       // product regular price changed
 
-      const diff: number = calculateDiff(oldItems[name].promo, newItems[name].promo)
+      const diff: number = calculateDiff(oldItems[name].regular, newItems[name].regular)
       await bot.send(`Продукт "${item.name}" змінив регулярну ціну на "${diff}". Попередня: ${oldItems[name].regular}. Тепер: ${newItems[name].regular}`)
-      item.changed = true
       updatedPrices.push(item)
-    } else if (oldItems[name].promo !== newItems[name].promo) {
+    } else if (oldItems[name].promo === 0 && newItems[name].promo !== 0) {
 
       const diff: number = calculateDiff(newItems[name].promo, newItems[name].regular)
       await bot.send(`Продукт "${item.name}" зайшов у промо "${diff}"`)
-      item.changed = true
       updatedPrices.push(item)
 
-    } else {
-
-      updatedPrices.push(item)
     }
+
+    item.changed = true
   }
 
   return updatedPrices
